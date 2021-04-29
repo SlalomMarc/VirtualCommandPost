40dde3878e02347b4c654663a43a52d3df0dc745
Marc Miller <prezbass@gmail.com>
2021-04-29 14:00:26 -0700

    added database connectivity

--- VirtualCommandPost.cs
+++ VirtualCommandPost.cs
@@ -15,6 +15,10 @@ namespace sdcs.vcp
         [FunctionName("VirtualCommandPost")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "vcpdb",
+                collectionName: "cases",
+                ConnectionStringSetting = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -24,6 +28,17 @@ namespace sdcs.vcp
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                // Add a JSON document to the output container.
+                await documentsOut.AddAsync(new
+                {
+                    // create a random ID
+                    id = System.Guid.NewGuid().ToString(),
+                    name = name
+                });
+            }
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
